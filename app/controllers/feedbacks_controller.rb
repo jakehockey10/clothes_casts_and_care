d552fe6517feacf1437eb28b8f717e663eb7a0f3
Jake Smith <jakehockey10@gmail.com>
2015-09-01 19:36:02 -0600

    fix route

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,6 +1,6 @@
 class FeedbacksController < ApplicationController
   before_action :set_feedback, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_admin!, only: [:index, :create, :edit, :update, :destroy]
+  before_action :authenticate_admin!, only: [:index, :edit, :update, :destroy]
 
   def index
     @feedbacks = Feedback.all
